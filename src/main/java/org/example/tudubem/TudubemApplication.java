0ff0ee440b70212e71c739f369c50008c19220ef
jinwoo.kim <luziatcode@gmail.com>
2026-02-20 16:45:02 +0900

    project init

---
+++ src/main/java/org/example/tudubem/TudubemApplication.java
@@ -0,0 +1,13 @@
+package org.example.tudubem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TudubemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TudubemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tudubem/TudubemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tudubem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TudubemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
